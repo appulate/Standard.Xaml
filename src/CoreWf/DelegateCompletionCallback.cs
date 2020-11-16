d1e8ac14de221c23b6985b778d17419fad4d9f07
Alexander Zhuravlev <azhuravlev@appulate.com>
2020-11-16 23:00:49 +0300

    remove all except system.xaml

--- src/CoreWf/DelegateCompletionCallback.cs
+++
@@ -1,9 +0,0 @@
-// This file is part of Core WF which is licensed under the MIT license.
-// See LICENSE file in the project root for full license information.
-
-namespace System.Activities
-{
-    using System.Collections.Generic;
-
-    public delegate void DelegateCompletionCallback(NativeActivityContext context, ActivityInstance completedInstance, IDictionary<string, object> outArguments);
-}
--- src/Test/TestCases.Runtime/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-// This file is part of Core WF which is licensed under the MIT license.
-// See LICENSE file in the project root for full license information.
-
-[assembly: Xunit.CollectionBehavior(Xunit.CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
